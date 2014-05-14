e20d602c9f580a077a5593b47f25829d4f1de38e
Hsuan Ho <hsuan@pixnet.tw>
2014-05-14 17:21:49 +0800

    add friend groups create example

---
+++ examples/512_friend_groups_create.php
@@ -0,0 +1,37 @@
+<?php
+require_once(__DIR__ . '/bootstrap.php');
+require_once(__DIR__ . '/include/checkAuth.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once(__DIR__ . '/include/header.php'); ?>
+</head>
+<body>
+<div class="container">
+    <?php require_once(__DIR__ . '/include/top.php'); ?>
+    <h1 class="page-header">新增好友群組</h1>
+    <h3>呼叫方式</h3>
+    <pre>$pixapi->friend->groups->create($name);</pre>
+    <h3>實際測試</h3>
+    <form class="form-inline" role="form" method="POST">
+      <div class="form-group">
+        <label class="sr-only" for="query">群組名稱(必填)</label>
+        <input type="text" class="form-control" id="query" name="query" placeholder="請輸入群組名稱">
+      </div>
+      <button type="submit" class="btn btn-primary">建立</button>
+    </form>
+    <?php
+        $query = $_POST['query'];
+        if ('' != $query) {
+    ?>
+    <h3>執行</h3>
+    <pre>$pixapi->friend->groups->create(<?= $query; ?>);</pre>
+    <h3>執行結果</h3>
+    <pre><?php print_r($pixapi->friend->groups->create($query)); ?></pre>
+    <?php
+        }
+    ?>
+</div>
+</body>
+</html>
