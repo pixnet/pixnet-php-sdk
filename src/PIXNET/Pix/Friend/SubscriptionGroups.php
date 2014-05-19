6e9fd89d1d528c6b8660516915130a35e4635ab9
Hsuan Ho <hsuan@pixnet.tw>
2014-05-19 19:03:12 +0800

    add search subscription_groups function

--- src/PIXNET/Pix/Friend/SubscriptionGroups.php
+++ src/PIXNET/Pix/Friend/SubscriptionGroups.php
@@ -10,4 +10,28 @@ class Pix_Friend_SubscriptionGroups extends PixAPI
     {
         $this->client = $client;
     }
+
+    public function search($group_id = '')
+    {
+        if ('' != $group_id) {
+            $parameters = $this->mergeParameters(
+                array(),
+                $options,
+                array(),
+                array()
+            );
+
+            $response = $this->query('friend/subscription_groups/' . $group_id, $parameters, 'GET');
+            return $response;
+        }
+        $parameters = $this->mergeParameters(
+            array(),
+            $options,
+            array(),
+            array()
+        );
+
+        $response = $this->query('friend/subscription_groups', $parameters, 'GET');
+        return $response;
+    }
 }
