b9fea680e389c689bfe4b51c72f5353667692888
Hsuan Ho <hsuan@pixnet.tw>
2014-05-19 18:58:28 +0800

    add delete subscriptions example

---
+++ examples/535_friend_subscriptions_delete.php
@@ -0,0 +1,53 @@
+<?php
+require_once(__DIR__ . '/bootstrap.php');
+require_once(__DIR__ . '/include/checkAuth.php');
+$name = '';
+$query = $_POST['query'];
+if ('' != $query) {
+  $response = $pixapi->friend->subscriptions->delete($query);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once(__DIR__ . '/include/header.php'); ?>
+</head>
+<body>
+<div class="container">
+    <?php require_once(__DIR__ . '/include/top.php'); ?>
+    <h1 class="page-header">刪除訂閱</h1>
+    <h3>呼叫方式</h3>
+    <pre>$pixapi->friend->subscriptions->delete($name);</pre>
+    <h3>實際測試</h3>
+    <form class="form-inline" role="form" method="POST">
+      <div class="form-group">
+
+          <select class="form-control" id="query" name="query">
+          <?php
+          $subscriptions = $pixapi->friend->subscriptions->search();
+          foreach ($subscriptions['subscriptions'] as $subscription) {
+              if ('' != $subscription['user']['name']) {
+                  $name = $subscription['user']['name'];
+?>
+                  <option value="<?= $name ?>" <?= ($query == $name) ? 'selected' : ''; ?>><?= $name ?></option>
+<?php
+              }
+          }
+          ?>
+          </select>
+      </div>
+      <button type="submit" class="btn btn-danger">刪除</button>
+    </form>
+    <?php
+        if ('' != $query) {
+    ?>
+    <h3>執行</h3>
+    <pre>$pixapi->friend->subscriptions->delete(<?= htmlspecialchars($query) ?>);</pre>
+    <h3>執行結果</h3>
+    <pre><?php print_r($response); ?></pre>
+    <?php
+        }
+    ?>
+</div>
+</body>
+</html>
