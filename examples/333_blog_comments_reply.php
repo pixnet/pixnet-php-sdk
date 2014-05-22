938e9eb649c30266ed2b9f2da5e89a4e7985dbf5
arrack <arrack@pixnet.tw>
2014-05-23 00:08:03 +0800

    wording

--- examples/333_blog_comments_reply.php
+++ examples/333_blog_comments_reply.php
@@ -33,7 +33,7 @@ require_once(__DIR__ . '/include/checkAuth.php');
         </div>
       </div>
       <div class="form-group">
-        <label class="col-sm-2 control-label" for="query">主文(必填)</label>
+        <label class="col-sm-2 control-label" for="query">內文(必填)</label>
         <div class="col-sm-10">
             <textarea class="form-control" id="query2" name="query2" placeholder="請輸入內文"></textarea>
         </div>
