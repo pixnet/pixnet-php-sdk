42acfecabb7bb502cefd6a202ff153ba17a8ab31
arrack <arrack@pixnet.tw>
2014-05-23 14:40:57 +0800

    wording

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    trigger_error('you need run Composer install first', E_USER_ERROR);
+    trigger_error('you need run composer install first', E_USER_ERROR);
 }
 require_once(__DIR__ . '/../vendor/autoload.php');
