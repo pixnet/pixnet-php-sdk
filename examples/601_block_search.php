4963a3f2524643110859ac233f6682115db3b56e
arrack <arrack@pixnet.tw>
2014-05-23 14:34:49 +0800

    add 601_block_search.php

---
+++ examples/601_block_search.php
@@ -0,0 +1,20 @@
+<?php
+require_once(__DIR__ . '/bootstrap.php');
+require_once(__DIR__ . '/include/checkAuth.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once(__DIR__ . '/include/header.php'); ?>
+</head>
+<body>
+<div class="container">
+    <?php require_once(__DIR__ . '/include/top.php'); ?>
+    <h1 class="page-header">列出黑名單</h1>
+    <h3>呼叫方式</h3>
+    <pre>$pixapi->block->search();</pre>
+    <h3>執行結果</h3>
+    <pre><?php print_r($pixapi->block->search()); ?></pre>
+</div>
+</body>
+</html>
