fc46a51d2c56991f19507c954cb115ac7bf4af41
arrack <arrack@pixnet.tw>
2014-05-23 11:59:37 +0800

    check vendor dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
-require_once(__DIR__ . '/../vendor/autoload.php')
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    trigger_error('you need run Composer install first', E_USER_ERROR);
+}
+require_once(__DIR__ . '/../vendor/autoload.php');
