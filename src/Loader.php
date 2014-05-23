868a0a58fd77ecf50ccc2e264e4e16ec10c331cd
arrack <arrack@pixnet.tw>
2014-05-23 14:47:00 +0800

    remove ini_set and error_reporting

--- src/Loader.php
+++ src/Loader.php
@@ -4,9 +4,6 @@
  * All rights reserved.
  */
 
-ini_set('display_errors', 'on');
-error_reporting(E_ALL ^ (E_NOTICE | E_STRICT | E_DEPRECATED));
-
 call_user_func(function () {
     set_include_path(implode(PATH_SEPARATOR, array(
         get_include_path(),
