3e66fc82176d66f904fbb6953e1649a84c3ee612
arrack <arrack@pixnet.tw>
2014-05-27 21:45:09 +0800

    add test for mainpage

---
+++ tests/PIXNET/Pix/MainpageTest.php
@@ -0,0 +1,25 @@
+<?php
+class Pix_MainpageTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    public static function tearDownAfterClass()
+    {
+        Authentication::tearDownAfterClass();
+    }
+
+    public function testSubClassIsExist()
+    {
+        $actual = self::$pixapi->mainpage;
+        $this->assertEquals('Pix_Mainpage_Blog', get_class($actual->blog));
+        $this->assertEquals('Pix_Mainpage_Album', get_class($actual->album));
+        $this->assertEquals('Pix_Mainpage_Video', get_class($actual->video));
+    }
+}
