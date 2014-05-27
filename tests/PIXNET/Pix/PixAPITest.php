5b3b68d9d704e1e03ae3dc24ceb0b98a950b1de9
arrack <arrack@pixnet.tw>
2014-05-27 21:52:07 +0800

    add test for PixAPI

---
+++ tests/PIXNET/Pix/PixAPITest.php
@@ -0,0 +1,30 @@
+<?php
+class PixAPITest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    public static function tearDownAfterClass()
+    {
+        Authentication::tearDownAfterClass();
+    }
+
+    public function testSubClassIsExist()
+    {
+        $actual = self::$pixapi;
+        $this->assertEquals('Pix_Index', get_class($actual->index));
+        $this->assertEquals('Pix_User', get_class($actual->user));
+        $this->assertEquals('Pix_Blog', get_class($actual->blog));
+        $this->assertEquals('Pix_Album', get_class($actual->album));
+        $this->assertEquals('Pix_Friend', get_class($actual->friend));
+        $this->assertEquals('Pix_Guestbook', get_class($actual->guestbook));
+        $this->assertEquals('Pix_Block', get_class($actual->block));
+        $this->assertEquals('Pix_Mainpage', get_class($actual->mainpage));
+    }
+}
