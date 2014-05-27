2f8dcb5e37e5965c87dde5f4196b5ea4e6fe5c38
arrack <arrack@pixnet.tw>
2014-05-27 17:04:16 +0800

    require init.inc.php
    Conflicts:
    	tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    trigger_error('you need run composer install first', E_USER_ERROR);
+    trigger_error('You need run composer install first', E_USER_ERROR);
+}
+if (!file_exists(__DIR__ . '/Authentication.php')) {
+    trigger_error('You must create a Authentication.php file from Authentication.sample.php', E_USER_ERROR);
 }
 require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/Authentication.php');
