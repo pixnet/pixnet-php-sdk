961b4813431817a65e2885cf7699710a2eed78f5
arrack <arrack@pixnet.tw>
2014-05-28 09:49:00 +0800

    if not found vendor/autoload.php use src/Loader.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    trigger_error('You need run composer install first', E_USER_ERROR);
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
+} else {
+    require_once(__DIR__ . '/../src/Loader.php');
 }
 if (!file_exists(__DIR__ . '/Authentication.php')) {
     trigger_error('You must create a Authentication.php file from Authentication.sample.php', E_USER_ERROR);
 }
-require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/Authentication.php');
