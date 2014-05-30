966bd22b736b3f12b092f290314a9a4d3a5c2832
Hsuan Ho <hsuan@pixnet.tw>
2014-05-30 18:26:55 +0800

    restyle

--- tests/PIXNET/Pix/FriendTest.php
+++ tests/PIXNET/Pix/FriendTest.php
@@ -28,20 +28,20 @@ class Pix_FriendTest extends PHPUnit_Framework_TestCase
         $actual_all = self::$pixapi->friend->news();
         foreach ($actual_all as $news) {
             $actual = array(
-                'content_type' => $news['content_type'],
-                'wording' => $news['wording'],
+                'content_type'      => $news['content_type'],
+                'wording'           => $news['wording'],
                 'blogarticle_title' => $news['blog_article']['title'],
-                'blogarticle_body' => $news['blog_article']['body'],
-                'user_name' => $news['user']['name']
+                'blogarticle_body'  => $news['blog_article']['body'],
+                'user_name'         => $news['user']['name']
             );
         }
 
         $expected = array(
-            'content_type' => 'blog',
-            'wording' => '文章更新',
+            'content_type'      => 'blog',
+            'wording'           => '文章更新',
             'blogarticle_title' => 'test new',
-            'blogarticle_body' => 'test',
-            'user_name' => 'emmatest2'
+            'blogarticle_body'  => 'test',
+            'user_name'         => 'emmatest2'
         );
 
         $this->assertEquals($expected, $actual);
