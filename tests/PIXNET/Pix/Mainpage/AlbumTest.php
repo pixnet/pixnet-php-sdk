ea42793322a967d89737ef7d3dfc7597314c10da
arrack <arrack@pixnet.tw>
2014-05-30 21:03:31 +0800

    add test for PixAPIException::REQUIRE_PARAMETERS_MISSING

--- tests/PIXNET/Pix/Mainpage/AlbumTest.php
+++ tests/PIXNET/Pix/Mainpage/AlbumTest.php
@@ -26,15 +26,45 @@ class Pix_Mainpage_AlbumTest extends PHPUnit_Framework_TestCase
         $this->assertCount(10, $actual['sets']);
     }
 
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testHotException()
+    {
+        $actual = self::$pixapi->mainpage->album->hot('');
+        $this->assertCount(10, $actual);
+    }
+
     public function testLatest()
     {
         $actual = self::$pixapi->mainpage->album->latest(0, array('api_version' => 2, 'count' => 10));
         $this->assertCount(10, $actual['sets']);
     }
 
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testLatestException()
+    {
+        $actual = self::$pixapi->mainpage->album->hot('');
+        $this->assertCount(10, $actual);
+    }
+
     public function testHotWeekly()
     {
         $actual = self::$pixapi->mainpage->album->hot_weekly(0, array('api_version' => 2, 'count' => 10));
         $this->assertCount(10, $actual['sets']);
+    }
+
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testHotWeeklyException()
+    {
+        $actual = self::$pixapi->mainpage->album->hot('');
+        $this->assertCount(10, $actual);
     }
 }
--- tests/PIXNET/Pix/Mainpage/BlogTest.php
+++ tests/PIXNET/Pix/Mainpage/BlogTest.php
@@ -26,15 +26,45 @@ class Pix_Mainpage_BlogTest extends PHPUnit_Framework_TestCase
         $this->assertCount(10, $actual);
     }
 
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testHotException()
+    {
+        $actual = self::$pixapi->mainpage->blog->hot('');
+        $this->assertCount(10, $actual);
+    }
+
     public function testLatest()
     {
         $actual = self::$pixapi->mainpage->blog->latest(0);
         $this->assertCount(10, $actual);
     }
 
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testLatestException()
+    {
+        $actual = self::$pixapi->mainpage->blog->hot('');
+        $this->assertCount(10, $actual);
+    }
+
     public function testHotWeekly()
     {
         $actual = self::$pixapi->mainpage->blog->hot_weekly(0);
+        $this->assertCount(10, $actual);
+    }
+
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::REQUIRE_PARAMETERS_MISSING
+     */
+    public function testHotWeeklyException()
+    {
+        $actual = self::$pixapi->mainpage->blog->hot('');
         $this->assertCount(10, $actual);
     }
 }
