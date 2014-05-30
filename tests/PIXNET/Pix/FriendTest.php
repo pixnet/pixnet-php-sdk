e1bf0f54ae2bd2c5e3ead7f851b0ced65c12622d
Hsuan Ho <hsuan@pixnet.tw>
2014-05-30 17:28:45 +0800

    add friend test

---
+++ tests/PIXNET/Pix/FriendTest.php
@@ -0,0 +1,49 @@
+<?php
+class Pix_FriendTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+    public static $group_id;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    public static function tearDownAfterClass()
+    {
+        Authentication::tearDownAfterClass();
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testGetException()
+    {
+        $actual = self::$pixapi->friend->subscription->search();
+    }
+
+    public function testNews()
+    {
+        $actual_all = self::$pixapi->friend->news();
+        foreach ($actual_all as $news) {
+            $actual = array(
+                'content_type' => $news['content_type'],
+                'wording' => $news['wording'],
+                'blogarticle_title' => $news['blog_article']['title'],
+                'blogarticle_body' => $news['blog_article']['body'],
+                'user_name' => $news['user']['name']
+            );
+        }
+
+        $expected = array(
+            'content_type' => 'blog',
+            'wording' => '文章更新',
+            'blogarticle_title' => 'test new',
+            'blogarticle_body' => 'test',
+            'user_name' => 'emmatest2'
+        );
+
+        $this->assertEquals($expected, $actual);
+    }
+}
