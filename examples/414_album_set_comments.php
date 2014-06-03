5a5823362308d28ba6c5a62bc8a3daff35285276
bency <bency@pixnet.tw>
2014-06-03 10:52:09 +0800

    加入取得相簿留言的範例

---
+++ examples/414_album_set_comments.php
@@ -0,0 +1,58 @@
+<?php
+require_once(__DIR__ . '/bootstrap.php');
+require_once(__DIR__ . '/include/checkAuth.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once(__DIR__ . '/include/header.php'); ?>
+</head>
+<body>
+<div class="container">
+    <?php require_once(__DIR__ . '/include/top.php'); ?>
+    <h1 class="page-header">取得相簿留言</h1>
+    <h3>呼叫方式</h3>
+    <pre>$pixapi->album->sets->comments($name, $set_id, <a href="http://developer.pixnet.pro/#!/doc/pixnetApi/albumSetComments" target="blank">$options</a> = array());</pre>
+    <div class="well">
+        <p>必填參數</p>
+        <ul>
+        <li>
+        <p>name</p>
+        <p>使用者名稱</p>
+        </li>
+        <li>
+        <p>set_id</p>
+        <p>相簿 id</p>
+        </li>
+        </ul>
+        <p>選填參數</p>
+        <ul>
+        <li><p>format</p>
+        <p>格式, 支援 xml 與 json. 預設為 json</p></li>
+        <li><p>page</p>
+        <p>頁數, 預設為1</p></li>
+        <li><p>per_page</p>
+        <p>每頁幾筆, 預設為100</p></li>
+        <li><p>password</p>
+        <p>相簿密碼，當使用者相簿設定為密碼相簿時使用</p></li>
+        </ul>
+    </div>
+    <h3>實際測試</h3>
+    <form class="form-inline" role="form" method="POST">
+      <div class="form-group">
+        <label class="sr-only" for="query">使用者名稱(必填)</label>
+        <input type="text" class="form-control" id="query" name="name" placeholder="請輸入使用者名稱" value="<?= $_POST['name']? $_POST['id'] : 'emmademo' ?>">
+        <label class="sr-only" for="query">相簿 id(必填)</label>
+        <input type="text" class="form-control" id="query" name="id" placeholder="請輸入相簿id" value="<?= $_POST['id']? $_POST['id'] : '34258' ?>">
+      </div>
+      <button type="submit" class="btn btn-primary">取得相簿內所有留言</button>
+    </form>
+    <?php if (!empty($_POST['name']) and !empty($_POST['id'])) {?>
+    <h3>實際執行</h3>
+    <pre>$pixapi->album->sets->comments(<?= htmlspecialchars($_POST['name'])?>, <?= $_POST['id'] ?>, $options)</pre>
+    <h3>執行結果</h3>
+    <pre><?php print_r($pixapi->album->sets->comments($_POST['name'], $_POST['id'])); ?></pre>
+    <?php }?>
+</div>
+</body>
+</html>
