71e8b81e84722a4ceb02bdef2edea065ab74159d
bency <bency@pixnet.tw>
2014-06-11 14:51:48 +0800

    sdk 也移除 format 的選項

--- src/PIXNET/Pix/Album/Sets.php
+++ src/PIXNET/Pix/Album/Sets.php
@@ -28,7 +28,7 @@ class Pix_Album_Sets extends PixAPI
             array('user' => $name),
             $options,
             array('parent_id', 'trim_user', 'page', 'per_page'),
-            array('format')
+            array()
         );
         if (empty($set_id)) {
             $response = $this->query('album/sets', $parameters, 'GET');
@@ -48,7 +48,7 @@ class Pix_Album_Sets extends PixAPI
             array('user' => $name, 'set_id' => $set_id),
             $options,
             array('page', 'per_page', 'with_detail', 'trim_user', 'iframe_width', 'iframe_height'),
-            array('type', 'password', 'format', 'use_iframe')
+            array('type', 'password', 'use_iframe')
         );
         $response = $this->query('album/elements', $parameters, 'GET');
         return $this->getResult($response, 'elements');
@@ -63,7 +63,7 @@ class Pix_Album_Sets extends PixAPI
             array('user' => $name, 'set_id' => $set_id),
             $options,
             array('page', 'per_page'),
-            array('password', 'format')
+            array('password')
         );
         $response = $this->query('album/set_comments', $parameters, 'GET');
         return $this->getResult($response, 'comments');
@@ -78,7 +78,7 @@ class Pix_Album_Sets extends PixAPI
             array('user' => $name, 'lat' => $lat, 'lon' => $lon),
             $options,
             array('page', 'per_page', 'trim_user', 'distance_max', 'distance_min'),
-            array('format')
+            array()
         );
         $response = $this->query('album/sets/nearby', $parameters, 'GET');
         return $this->getResult($response, 'sets');
@@ -93,7 +93,7 @@ class Pix_Album_Sets extends PixAPI
             array('title' => $title, 'description' => $desc),
             $options,
             array('permission', 'category_id', 'is_lockright', 'allow_cc', 'cancomment', 'allow_commercial_usr', 'allow_derivation', 'parent_id'),
-            array('format', 'password', 'password_hint', 'friend_group_ids')
+            array('password', 'password_hint', 'friend_group_ids')
         );
         $response = $this->query('album/sets', $parameters, 'POST');
         return $this->getResult($response, 'set');
@@ -108,7 +108,7 @@ class Pix_Album_Sets extends PixAPI
             array('title' => $title, 'description' => $desc),
             $options,
             array('permission', 'category_id', 'is_lockright', 'allow_cc', 'cancomment', 'allow_commercial_usr', 'allow_derivation', 'parent_id'),
-            array('format', 'password', 'password_hint', 'friend_group_ids')
+            array('password', 'password_hint', 'friend_group_ids')
         );
         $response = $this->query('album/sets/' . $set_id, $parameters, 'POST');
         return $this->getResult($response, 'set');
@@ -123,7 +123,7 @@ class Pix_Album_Sets extends PixAPI
             array('parent_id' => $parent_id, 'ids' => $ids),
             $options,
             array(),
-            array('format')
+            array()
         );
         $response = $this->query('album/sets/position', $parameters, 'POST');
         return $this->getResult($response, 'sets');
@@ -138,7 +138,7 @@ class Pix_Album_Sets extends PixAPI
             array(),
             $options,
             array(),
-            array('format')
+            array()
         );
         $response = $this->query('album/sets/' . $set_id, $parameters, 'DELETE');
         return $response;
