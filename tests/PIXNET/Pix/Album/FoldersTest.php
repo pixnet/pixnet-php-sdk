bf7a2bcb14b6baee0b408aee8109b0e3c5e18714
bency <bency@pixnet.tw>
2014-06-16 18:06:50 +0800

    album->folders->create test

--- tests/PIXNET/Pix/Album/FoldersTest.php
+++ tests/PIXNET/Pix/Album/FoldersTest.php
@@ -25,4 +25,14 @@ class Pix_FoldersTest extends PHPUnit_Framework_TestCase
         $actual = $ret['id'];
         $this->assertEquals($expected, $actual);
     }
+
+    public function testCreate()
+    {
+        $expected_title = 'PIXNET_SDK_TITLE';
+        $expected_desc = 'PIXNET_SDK_DESC';
+        $tempFolder = self::$pixapi->album->folders->create($expected_title, $expected_desc);
+        $this->assertEquals($expected_title, $tempFolder['title']);
+        $this->assertEquals($expected_desc, $tempFolder['description']);
+        self::$pixapi->album->folders->delete($tempFolder['id']);
+    }
 }
