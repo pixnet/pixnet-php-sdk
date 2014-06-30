fad3121922151249bcdc5f9b50d25133b64079fc
bency <bency@pixnet.tw>
2014-06-30 11:35:46 +0800

    修改範例顯示的變數名稱

--- examples/428_album_sets_position.php
+++ examples/428_album_sets_position.php
@@ -29,7 +29,7 @@ if (isset($_GET['folder_id'])) {
     <?php require_once(__DIR__ . '/include/top.php'); ?>
     <h1 class="page-header">修改相簿資料夾內的相簿順序</h1>
     <h3>呼叫方式</h3>
-    <pre>$pixapi->album->sets->position($folder_id, $ids);</pre>
+    <pre>$pixapi->album->sets->position($parent_id, $ids);</pre>
     <div class="well">
         <p>必填參數</p>
         <ul>
