c64f75b1fba59c69299ebde4219fe58d65f91f4f
arrack <arrack@pixnet.tw>
2014-07-01 11:35:59 +0800

    增加主旨

--- examples/703_guestbook_create.php
+++ examples/703_guestbook_create.php
@@ -23,6 +23,12 @@ require_once(__DIR__ . '/include/checkAuth.php');
         </div>
       </div>
       <div class="form-group">
+        <label class="col-sm-2 control-label" for="title">主旨(必填)</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="title" name="title" placeholder="請輸入主旨">
+        </div>
+      </div>
+      <div class="form-group">
         <label class="col-sm-2 control-label" for="query">內文(必填)</label>
         <div class="col-sm-10">
             <textarea class="form-control" id="query2" name="query2" placeholder="請輸入內文"></textarea>
@@ -39,12 +45,13 @@ require_once(__DIR__ . '/include/checkAuth.php');
     <?php
         $query = $_POST['query'];
         $query2 = $_POST['query2'];
-        if ('' != $query and '' != $query2) {
+        $title = $_POST['title'];
+        if ('' != $query and '' != $query2 and '' != $title) {
     ?>
     <h3>執行</h3>
-    <pre>$pixapi->guestbook->create('<?= $query; ?>', '<?= $query2; ?>');</pre>
+    <pre>$pixapi->guestbook->create('<?= $query; ?>', '<?= $title; ?>', '<?= $query2; ?>');</pre>
     <h3>執行結果</h3>
-    <pre><?php print_r($pixapi->guestbook->create($query, $query2)); ?></pre>
+    <pre><?php print_r($pixapi->guestbook->create($query, $title, $query2)); ?></pre>
     <?php
         }
     ?>
