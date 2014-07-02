962d25ef5f56edbe4c7f6116e69d93ed78618c61
arrack <arrack@pixnet.tw>
2014-07-02 16:30:13 +0800

    add Block Test

---
+++ tests/PIXNET/Pix/BlockTest.php
@@ -0,0 +1,63 @@
+<?php
+class Pix_Block_GroupsTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    public static function tearDownAfterClass()
+    {
+        Authentication::tearDownAfterClass();
+    }
+
+    /**
+     * @group test
+     */
+    public function testCreate()
+    {
+        $actual_all = self::$pixapi->block->create('emma');
+        $actual = $actual_all['block']['user']['name'];
+
+        $this->assertEquals('emma', $actual);
+    }
+
+    /**
+     * @expectedException PixAPIException
+     */
+    public function testCreateException()
+    {
+        $actual = self::$pixapi->block->create('');
+    }
+
+    public function testSearch()
+    {
+        $actual_all = self::$pixapi->block->search();
+        $actual = $actual_all[0]['user']['name'];
+        $expected = 'emma';
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @group test
+     */
+    public function testDelete()
+    {
+        self::$pixapi->block->delete('emma');
+        $actual = self::$pixapi->block->search();
+
+        $this->assertFalse($actual);
+    }
+
+    /**
+     * @expectedException PixAPIException
+     */
+    public function testDeleteException()
+    {
+        $actual = self::$pixapi->block->delete('');
+    }
+}
