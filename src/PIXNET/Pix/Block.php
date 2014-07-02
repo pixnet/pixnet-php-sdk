7265be75c2795bfae4cca8c2e2428060508ab300
arrack <arrack@pixnet.tw>
2014-07-02 17:52:32 +0800

    check sub class not found

--- src/PIXNET/Pix/Block.php
+++ src/PIXNET/Pix/Block.php
@@ -11,6 +11,11 @@ class Pix_Block extends PixAPI
         $this->client = $client;
     }
 
+    public function __get($class_name)
+    {
+        throw new PixAPIException('CLASS [' . $class_name . '] NOT FOUND', PixAPIException::CLASS_NOT_FOUND);
+    }
+
     public function search()
     {
         $response = $this->query('blocks');
