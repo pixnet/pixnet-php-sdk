f042bed08d68a3a37e1a8f0264f828f7c02d6df9
bency <bency@pixnet.tw>
2014-07-08 18:28:06 +0800

    繼承 Pix_Comments 的基本設定

--- src/PIXNET/Pix/Album/Elements/Comments.php
+++ src/PIXNET/Pix/Album/Elements/Comments.php
@@ -4,11 +4,12 @@
  * All rights reserved.
  */
 
-class Pix_Album_Elements_Comments extends PixAPI
+class Pix_Album_Elements_Comments extends Pix_Comments
 {
     public function __construct($client)
     {
         $this->client = $client;
+        $this->setApiPath('album/comments');
     }
 
     public function __get($class_name)
