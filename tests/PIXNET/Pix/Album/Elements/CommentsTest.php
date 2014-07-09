97e1619e48a2c5c29e43b1e93e262bb1c34b734f
bency <bency@pixnet.tw>
2014-07-09 18:18:25 +0800

    加入基本共用函式

--- tests/PIXNET/Pix/Album/Elements/CommentsTest.php
+++ tests/PIXNET/Pix/Album/Elements/CommentsTest.php
@@ -10,6 +10,24 @@ class Pix_Album_Element_CommentsTest extends PHPUnit_Framework_TestCase
         Authentication::setUpBeforeClass();
         self::$pixapi = Authentication::$pixapi;
         self::$test_set = self::$pixapi->Album->Sets->search('emmatest')[0];
-        self::$test_element = self::$pixapi->album->sets->elements('emmatest', self::$test_set['id']);
+        self::$test_element = self::$pixapi->album->sets->elements('emmatest', self::$test_set['id'])[0];
+    }
+
+    private function createTempComments()
+    {
+        $comments = [];
+        for ($i = 0; $i < 5; $i++) {
+            $comments[$i] = self::$pixapi->album->elements->comments->create('emmatest', self::$test_element['id'], 'test message');
+            echo "create " . $comments[$i]['id'] . PHP_EOL;
+        }
+        return $comments;
+    }
+
+    private function destoryTempComments($comments)
+    {
+        foreach ($comments as $c) {
+            echo "delete " . $c['id'] . PHP_EOL;
+            self::$pixapi->album->elements->comments->delete($c['id']);
+        }
     }
 }
