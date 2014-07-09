09e590a89c1bbad18077a21ed18c0b325581cae3
bency <bency@pixnet.tw>
2014-07-09 17:03:32 +0800

    add create test

--- tests/PIXNET/Pix/Album/CommentsTest.php
+++ tests/PIXNET/Pix/Album/CommentsTest.php
@@ -61,4 +61,22 @@ class Pix_Album_commentsTest extends PHPUnit_Framework_TestCase
 
         $this->destoryTempComments($current);
     }
+
+    /**
+     * @expectedException PixAPIException
+     */
+    public function testCreateException()
+    {
+        self::$pixapi->album->comments->create('', '', '');
+    }
+
+    public function testCreate()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $body = md5($i);
+            $comment = self::$pixapi->Album->comments->create('emmatest', self::$test_set['id'], $body);
+            $this->assertEquals($body, $comment['body']);
+            self::$pixapi->Album->comments->delete($comment['id']);
+        }
+    }
 }
