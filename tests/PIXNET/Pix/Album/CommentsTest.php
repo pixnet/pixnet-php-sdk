e6f3b04bc400aa978a02b1b2a8a2414c557d11fb
bency <bency@pixnet.tw>
2014-07-09 14:49:47 +0800

    建立 album comment 測試基本架構

---
+++ tests/PIXNET/Pix/Album/CommentsTest.php
@@ -0,0 +1,11 @@
+<?php
+class Pix_Album_commentsTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+}
