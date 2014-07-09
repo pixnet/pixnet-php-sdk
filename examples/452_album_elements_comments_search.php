614d91fd319d9aa6fe0409064fa30976307f6cf6
bency <bency@pixnet.tw>
2014-07-09 18:16:25 +0800

    修正範例的使用方式

--- examples/452_album_elements_comments_search.php
+++ examples/452_album_elements_comments_search.php
@@ -2,16 +2,6 @@
 require_once(__DIR__ . '/bootstrap.php');
 require_once(__DIR__ . '/include/checkAuth.php');
 $name = $pixapi->getUserName();
-$sets = $pixapi->album->sets->search($name);
-foreach ($sets as $k => $set) {
-    $count = $pixapi->album->sets->elements($name, $set['id']) ? count($pixapi->album->sets->elements($name, $set['id'])) : 0;
-    $sets[$k]['title'] .= " ( $count )";
-}
-if (!isset($_POST['set_id'])) {
-    $current_set = $sets[0];
-} else {
-    $current_set = $pixapi->album->sets->search($name, ['set_id' => $_POST['set_id']]);
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,17 +13,13 @@ if (!isset($_POST['set_id'])) {
     <?php require_once(__DIR__ . '/include/top.php'); ?>
     <h1 class="page-header">取得相簿上所有留言</h1>
     <h3>呼叫方式</h3>
-    <pre>$pixapi->album->elements->comments->search($name, ['element_id' => $element_id], $options = [])</pre>
+    <pre>$pixapi->album->elements->comments->search($name, [], $options = [])</pre>
     <div class="well">
         <p>必填參數</p>
         <ul>
             <li>
                 <p>name</p>
                 <p>使用者名稱</p>
-            </li>
-            <li>
-                <p>set_id</p>
-                <p>相簿 id</p>
             </li>
         </ul>
         <p>選填參數</p>
@@ -52,30 +38,10 @@ if (!isset($_POST['set_id'])) {
             </li>
         </ul>
     </div>
-    <h3><a href="#execute" name="execute">實際測試</a></h3>
-    <form action="#execute" class="form-horizontal" role="form" method="POST">
-      <div class="form-group">
-        <label class="col-sm-2 control-label" for="query">請選擇相簿</label>
-        <div class="col-sm-5">
-            <select class="form-control" id="query" name="set_id">
-                <?php foreach ($sets as $set) { ?>
-                    <?php if ($set['id'] == $current_set['id']) {?>
-                <option value="<?= $set['id']?>" selected><?= $set['title']?></option>
-                    <?php } else {?>
-                <option value="<?= $set['id']?>"><?= $set['title']?></option>
-                    <?php } ?>
-                <?php } ?>
-            </select>
-        </div>
-      </div>
-      <button type="submit" class="btn btn-primary">取得照片留言</button>
-    </form>
-    <?php if (!empty($_POST['set_id'])) {?>
     <h3>實際執行</h3>
-    <pre>$pixapi->album->elements->comments->search(<?= $name ?>, ['set_id' => <?= $_POST['set_id'] ?>], $options)</pre>
+    <pre>$pixapi->album->elements->comments->search('<?= $name ?>', [], $options)</pre>
     <h3>執行結果</h3>
-    <pre><?php print_r($pixapi->album->elements->comments->search($name, ['set_id' => $_POST['set_id']])); ?></pre>
-    <?php }?>
+    <pre><?php print_r($pixapi->album->elements->comments->search($name, [])); ?></pre>
 </div>
 </body>
 </html>
