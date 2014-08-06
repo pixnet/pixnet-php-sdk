b4a79af851afc3010ea02ffcc879ea77a8e6a250
arrack <arrack@pixnet.tw>
2014-08-06 18:24:35 +0800

    add Pix_Blog_ArticlesTest

---
+++ tests/PIXNET/Pix/Blog/ArticlesTest.php
@@ -0,0 +1,20 @@
+<?php
+class Pix_Blog_ArticlesTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    /**
+     * @expectedException PixAPIException
+     * @expectedExceptionCode PixAPIException::CLASS_NOT_FOUND
+     */
+    public function testSubClassNotFoundException()
+    {
+        $actual = self::$pixapi->blog->articles->notfound;
+    }
+}
