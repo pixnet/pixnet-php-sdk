739f0349c6dfa4953ec0a48b56d57de9caa3e1a3
bency <bency@pixnet.tw>
2014-08-16 00:13:48 +0800

    blog comments unit test initialize

---
+++ tests/PIXNET/Pix/Blog/CommentsTest.php
@@ -0,0 +1,11 @@
+<?php
+class Pix_Blog_CommentsTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+}
