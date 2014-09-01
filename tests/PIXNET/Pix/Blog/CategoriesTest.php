192d9a78c18436af033e45d752de1004ab36c810
bency <bency@pixnet.tw>
2014-09-02 00:35:01 +0800

    blog categories test initailize

---
+++ tests/PIXNET/Pix/Blog/CategoriesTest.php
@@ -0,0 +1,16 @@
+<?php
+class Pix_Blog_CategoriesTest extends PHPUnit_Framework_TestCase
+{
+    public static $pixapi;
+
+    public static function setUpBeforeClass()
+    {
+        Authentication::setUpBeforeClass();
+        self::$pixapi = Authentication::$pixapi;
+    }
+
+    public static function tearDownAfterClass()
+    {
+        Authentication::tearDownAfterClass();
+    }
+}
