e94228df7ea7735ecdeed1b845553acd96a6d0d2
bency <bency@pixnet.tw>
2014-09-04 13:49:18 +0800

    modify categories search example

--- examples/311_blog_categories_search.php
+++ examples/311_blog_categories_search.php
@@ -12,9 +12,9 @@ require_once(__DIR__ . '/include/checkAuth.php');
     <?php require_once(__DIR__ . '/include/top.php'); ?>
     <h1 class="page-header">取得部落格所有分類</h1>
     <h3>呼叫方式</h3>
-    <pre>$pixapi->blog->categories->search();</pre>
+    <pre>$pixapi->blog->categories->search('emmatest');</pre>
     <h3>執行結果</h3>
-    <pre><?php print_r($pixapi->blog->categories->search()); ?></pre>
+    <pre><?php print_r($pixapi->blog->categories->search('emmatest')); ?></pre>
 </div>
 </body>
 </html>
