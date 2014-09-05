79a24d6a99c68673f7f8f8416d3ac4ac36053c21
arrack <arrack@pixnet.tw>
2014-09-05 14:46:38 +0800

    add title

--- examples/include/header.php
+++ examples/include/header.php
@@ -1,6 +1,7 @@
 <?php
     require_once(__DIR__ . '/example_list.php');
 ?>
+<title>PIXNET PHP SDK EXAMPLE</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="include/style.css" rel="stylesheet">
