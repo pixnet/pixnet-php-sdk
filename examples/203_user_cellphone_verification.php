98e21b3d870f205ba3ea470407e1e0ef628053f9
arrack <arrack@pixnet.tw>
2014-12-29 14:01:45 +0800

    fix typo

--- examples/203_user_cellphone_verification.php
+++ examples/203_user_cellphone_verification.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . '/include/checkAuth.php');
     <?php require_once(__DIR__ . '/include/top.php'); ?>
     <h1 class="page-header">檢查使用者手機驗證狀態</h1>
     <h3>呼叫方式</h3>
-    <pre>$pixapi->user->info());</pre>
+    <pre>$pixapi->user->cellphoneVerification());</pre>
     <h3>執行結果</h3>
     <pre><?php var_dump($pixapi->user->cellphoneVerification()); ?></pre>
 </div>
