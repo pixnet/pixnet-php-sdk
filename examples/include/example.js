7032d774c390df3d17def0bb3d9395d3bf4e435b
nealhuang <nealhuang@pixnet.tw>
2016-11-01 16:33:17 +0800

    Add getGetParam function to example.js

---
+++ examples/include/example.js
@@ -0,0 +1,15 @@
+var getGetParam = function(search) {
+    if ('' === search) {
+        return [];
+    }
+    var querystring = search.substr(1);
+    var querys = querystring.split('&');
+    var obj = [];
+    for (var i = 0; i < querys.length; i++) {
+        var query = querys[i].split('=');
+        obj[query[0]] = query[1];
+
+    }
+
+    return obj;
+};
