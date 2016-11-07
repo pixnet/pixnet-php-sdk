5d7bfbe0294fe5ca03c71e5293208424d36e566d
nealhuang <nealhuang@pixnet.tw>
2016-11-07 18:39:29 +0800

    Add load source logic for custom css and js

--- examples/include/header.php
+++ examples/include/header.php
@@ -4,7 +4,13 @@
 <title>PIXNET PHP SDK EXAMPLE</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-<link href="../include/style.css" rel="stylesheet">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<script src="../include/example.js"></script>
+<?php
+if (!strpos($_SERVER['PHP_SELF'], "index")) {
+    ?>
+    <link href="../include/style.css" rel="stylesheet">
+    <script src="../include/example.js"></script>
+    <?php
+}
+?>
